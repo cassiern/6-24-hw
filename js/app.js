4f882576194511b75c9582dd2fa2e8b87ffde63a
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-24 18:47:00 -0600

    data structures

--- js/app.js
+++ js/app.js
@@ -5,3 +5,45 @@
 // Array
 // Object
 // Object with arrays
+
+//2.
+
+const rainbow = ["blue", "red", "yellow", "orange", "purple", "green"];
+rainbow[0];
+
+const cassie = {
+	favoriteFood: "chicken Scampi",
+	hobby: "rock climbing",
+	currentLocation: "Arvada",
+	favDatatype: "objects"
+}
+console.log(cassie.hobby);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
